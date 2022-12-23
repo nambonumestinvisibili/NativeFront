d6dece64222fefacb97c9c58d40805686bec10cc
Dawid H <dawid.holewa@axa.ie>
2022-12-23 01:01:14 +0100

    eslint

--- constants/colors.js
+++ constants/colors.js
@@ -1,14 +1,14 @@
 const colors = {
-    ACCENTS: {
-        PINK: "#CC96F5",
-        MINT: "#99EDCC",
-        BABYBLUE: "#1098F",
-        MUSTARD: "#FFD449",
-        BURGUND: "#9A275A"
-    },
-    BASIC: {
-        WHITE: "#FFFFFF"
-    }
+  ACCENTS: {
+    PINK: '#CC96F5',
+    MINT: '#99EDCC',
+    BABYBLUE: '#1098F',
+    MUSTARD: '#FFD449',
+    BURGUND: '#9A275A'
+  },
+  BASIC: {
+    WHITE: '#FFFFFF'
+  }
 }
 
 export default colors
